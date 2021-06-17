a43640a6031bf9dfe6dc85fbf55f637f13f45556
Michal Wilanowski <wilanowski.michal@gmail.com>
2021-06-17 12:54:22 +0200

    add hero and list

---
+++ src/components/Hero/hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+const Hero = function(){
+    const content = (
+      <header className={styles.component}>
+        <h2 className={styles.title}>Things to do</h2>
+        <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" className={styles.image}></img>
+      </header>
+    );
+
+    return content;
+  };
+
+  export default Hero;
